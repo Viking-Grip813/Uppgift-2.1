14a66222767cebecf8ae5dd40d2fe1baa19e3d90
vist07001 <vist07001@E-PW05CGR8>
2024-10-09 11:59:40 +0200

    Add project files.

---
+++ uppgift 2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Uppgift2_1
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            String bok = Console.ReadLine();
+        }
+    }
+}
